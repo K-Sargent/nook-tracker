35d5d49139705e500668623d42a5322d9923d133
beccazitting <rebeccazitting20@gmail.com>
2021-11-23 21:14:14 -0700

    make app nav bar have intuitive icons and correct names

--- app/src/main/java/com/example/nooktracker/ui/tasks/TasksViewModel.java
+++ app/src/main/java/com/example/nooktracker/ui/tasks/TasksViewModel.java
@@ -1,16 +1,16 @@
-package com.example.nooktracker.ui.dashboard;
+package com.example.nooktracker.ui.tasks;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class TasksViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public TasksViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is home fragment");
     }
 
     public LiveData<String> getText() {
--- app/src/main/java/com/example/nooktracker/ui/villagers/VillagersViewModel.java
+++ app/src/main/java/com/example/nooktracker/ui/villagers/VillagersViewModel.java
@@ -1,16 +1,16 @@
-package com.example.nooktracker.ui.home;
+package com.example.nooktracker.ui.villagers;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class VillagersViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public VillagersViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
